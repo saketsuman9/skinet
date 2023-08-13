0a8b41bb579dacb868964f9715147ca5df95a288
saletsuman9 <saketsuman9@gmail.com>
2023-08-13 16:21:19 +0530

    End of Section 2

---
+++ API/Controllers/ProductsController.cs
@@ -0,0 +1,32 @@
+using Core.Entities;
+using Infrastructure.Data;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace API.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class ProductsController : ControllerBase
+    {
+        private readonly StoreContext _context;
+        public ProductsController(StoreContext context)
+        {
+            _context = context;
+        }
+
+
+        [HttpGet]
+        public async Task<ActionResult<List<Product>>> GetProducts()
+        {          
+            var produucts = await _context.Products.ToListAsync();
+            return produucts;
+        }
+
+        [HttpGet("{id}")]
+        public async Task<ActionResult<Product>> GetProducts(int id)
+        {          
+            return await _context.Products.FindAsync(id);
+        }
+    }
+}
